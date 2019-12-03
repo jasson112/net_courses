c9795c5265642e3349c0348999d41b15703f8f3e
Jasson <jasson@impulsetravel.co>
2019-12-03 17:54:04 -0500

    update project !

---
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+
+        public ListModel(IRestaurantData restaurantData) {
+            this.restaurantData = restaurantData;
+        }
+
+        public void OnGet()
+        {
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
+        }
+    }
+}
